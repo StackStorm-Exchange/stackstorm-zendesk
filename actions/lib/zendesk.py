11d85a8913ed470d4c4cdff57bc1c2759a0c1ad7
Kirill Izotov <enykeev@stackstorm.com>
2017-10-27 14:55:06 +0700

    Remove deprecated st2actions import

--- actions/lib/zendesk.py
+++ actions/lib/zendesk.py
@@ -4,7 +4,7 @@ from zenpy.lib.api_objects import Comment
 from zenpy.lib.exception import RecordNotFoundException
 from zenpy.lib.exception import APIException
 
-from st2actions.runners.pythonrunner import Action
+from st2common.runners.base_action import Action
 
 __all__ = [
     'ZendeskAction'
